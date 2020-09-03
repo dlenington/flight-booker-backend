3e3869c95e725b37f67157c7ad7fc23264302d27
Dan Lenington <danlening02@gmail.com>
2020-09-03 09:07:35 -0500

    Defining structs for Graphql server

---
+++ go.mod
@@ -0,0 +1,9 @@
+module flight-booker-backend
+
+go 1.15
+
+require (
+	github.com/graphql-go/graphql v0.7.9
+	github.com/stamblerre/gocode v1.0.0 // indirect
+	rsc.io/quote v1.5.2 // indirect
+)
---
+++ hello.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/graphql-go/graphql"
+)
+
+type Tutorial struct {
+	ID		int
+	Title	string 
+	Author Author
+	Comments []Comment
+}
+
+type Author struct {
+	Name string
+	Tutorials []int
+}
+
+type Comment struct {
+	Body string
+}
+
+func main() {
+
+
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "World", nil
+			},
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("Failed to create new GraphQL Schema, err %v", err)
+	}
+
+	query := `
+	{
+		hello
+	}
+		`
+
+		params := graphql.Params{Schema: schema, RequestString: query}
+		r := graphql.Do(params)
+		if len(r.Errors) > 0 {
+			log.Fatalf("Failed to execute graphql operation, errors: %+v", r.Errors)
+		}
+
+		rJSON, _ := json.Marshal(r)
+		fmt.Printf("%s \n", rJSON)
+}
