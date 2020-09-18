3854ce9fc6fd9342dc899be9a8a69d4c7a138684
Dan Lenington <danlening02@gmail.com>
2020-09-18 09:55:24 -0500

    First query

--- main.go
+++ main.go
@@ -163,5 +163,17 @@ func main() {
 		create(title: "Hello World")  {
 			
 			
-			}`
+			}
+			`
+
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) >  0 {
+		log.Fatalf("Failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
+
+	//Query
 }
